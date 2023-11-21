c374d9da0b57c931889a5b9175b855d481206cb5
Henry Lam <hcylam@connect.ust.hk>
2023-11-21 15:20:26 +0800

    added logging to the program, changed the title of the program

--- OverwatchBlocker.py
+++ OverwatchBlocker.py
@@ -1,16 +1,21 @@
 import tkinter as tk
 import tkinter.font as tkFont
+from tkinter import scrolledtext
 import psutil
 import threading
 
 import time
+import datetime
 
 class OverwatchBlocker:
     def __init__(self):
         self.blocker = True
         self.window = tk.Tk()
+        self.window.title("Overwatch Blocker")
         self.thread = threading.Thread(target=self.bnet_blocker)
         self.status = tk.Label(self.window, text="Overwatch/Bnet is being blocked.")
+
+        self.logging_text = scrolledtext.ScrolledText(self.window, wrap=tk.WORD, width=500, height=300)
 
         self.start_app()
 
@@ -24,15 +29,15 @@ class OverwatchBlocker:
                     pid = program.pid
                     process = psutil.Process(pid)
                     process.terminate()
-                    print("Terminating " + str(program.name()))
+
+                    self.logging_text.insert(tk.END, datetime.datetime.now().strftime("%Y/%m/%d %H:%M:%S") + ": Blocking " + program.name() + "\n")
+                    self.logging_text.see(tk.END)
             time.sleep(15)
 
     def completed_drawing(self):
         self.blocker = False
         self.thread.join()
         self.status.config(text="Overwatch is not being blocked anymore. Have Fun!")
-
-        
 
     def start_app(self):
         self.thread.start()
@@ -54,9 +59,15 @@ class OverwatchBlocker:
 
         gap = tk.Label(self.window, text="")
         gap.pack()
-
-
         self.status.pack()
+
+        space2 = tk.Label(self.window, text="\n\n")
+        space2.pack()
+
+        logs_label = tk.Label(self.window, text="Logs", font=button_font)
+        logs_label.pack()
+
+        self.logging_text.pack(expand=True, fill=tk.BOTH)
 
 
 
